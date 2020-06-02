5c2f12a9c21dd77000ad0bd18b8caa20ac4dba77
Michael Houston <michael.houston3@nhs.net>
2020-06-02 10:38:41 +0100

    Avoid using spring-security 5.x class for backwards compatibility with Spring Boot 1.5

--- src/main/java/uk/nhs/cactus/common/security/CactusPrincipal.java
+++ src/main/java/uk/nhs/cactus/common/security/CactusPrincipal.java
@@ -1,14 +1,15 @@
 package uk.nhs.cactus.common.security;
 
+import java.security.Principal;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.security.core.AuthenticatedPrincipal;
 
 @AllArgsConstructor
 @Builder(toBuilder = true)
 @Data
-public class CactusPrincipal implements AuthenticatedPrincipal {
+public class CactusPrincipal implements Principal {
+
   private String name;
   private String supplierId;
 }

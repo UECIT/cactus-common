c2729d4834ea8653f059eeec356ff9a33e9957ab
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-07-23 15:03:03 +0100

    Updated local audit sender to save audits directly to an elasticsearch instance.

--- src/main/java/uk/nhs/cactus/common/audit/sender/LocalAuditSender.java
+++ src/main/java/uk/nhs/cactus/common/audit/sender/LocalAuditSender.java
@@ -1,16 +1,19 @@
 package uk.nhs.cactus.common.audit.sender;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestTemplate;
 import uk.nhs.cactus.common.audit.model.AuditSession;
+import uk.nhs.cactus.common.elasticsearch.ElasticSearchClient;
+import uk.nhs.cactus.common.security.TokenAuthenticationService;
 
 @Service
 @RequiredArgsConstructor
@@ -19,30 +22,29 @@ import uk.nhs.cactus.common.audit.model.AuditSession;
 public class LocalAuditSender implements AuditSender {
 
     private final ObjectMapper mapper;
-    private final RestTemplate auditRestTemplate;
-
-    @Value("${sqs.audit.queue}")
-    private String loggingQueue;
+    private final ElasticSearchClient elasticSearchClient;
+    private final TokenAuthenticationService authenticationService;
 
     @Value("${service.name}")
     private String serviceName;
 
     @Override
     public void sendAudit(AuditSession audit) {
-        if (StringUtils.isEmpty(loggingQueue)) {
-            logAudit(audit, "No audit server configured");
-            return;
-        }
+        var supplierId = authenticationService.requireSupplierId();
+        var metadata = Map.of(
+            "requestId", UUID.randomUUID(),
+            "@timestamp", Instant.now().toString(),
+            "@owner", serviceName);
 
         try {
-            auditRestTemplate.postForLocation(loggingQueue + "/send/{service}", audit, serviceName);
-        } catch (ResourceAccessException e) {
-            logAudit(audit, "Audit server configured but cannot connect");
+            elasticSearchClient.store(supplierId + "-audit", audit, metadata);
+        } catch (IOException e) {
+            logAudit(audit);
         }
     }
 
     @SneakyThrows
-    private void logAudit(AuditSession audit, String message) {
-        log.info(message + ": " + mapper.writeValueAsString(audit));
+    private void logAudit(AuditSession audit) {
+        log.info("Audit server configured but cannot connect: " + mapper.writeValueAsString(audit));
     }
 }
